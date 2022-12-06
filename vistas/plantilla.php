ea2e4ee9bdcfe5b530361253181ab0e62cc39320
Kurumi-007 <fly07sal@gmail.com>
2022-12-06 14:03:06 -0600

    archivos nuevo

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,4 +1,22 @@
-<?php
-
-echo"hola mundo";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo COMPANY; ?></title>
+	<?php include "inc/Link.php";?>
+</head>
+<body>
+	
+	<!-- Main container -->
+	<main class="full-box main-container">
+		<!-- Nav lateral -->
+		<?php include "inc/NavLateral.php";?>
+		<!-- Page content -->
+		<section class="full-box page-content">
+		<?php include "inc/NavBar.php";?>
+		</section>
+	</main>
+	<?php include "inc/Script.php";?>
+</body>
+</html>
