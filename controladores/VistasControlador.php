77b1f0362ec373f4a3c4aeb265b392e986dd6e56
Kurumi-007 <fly07sal@gmail.com>
2022-12-06 10:03:25 -0600

    Index actualizado

--- controladores/VistasControlador.php
+++ controladores/VistasControlador.php
@@ -2,5 +2,19 @@
     require_once "./modelos/VistasModelo.php";
     class VistasControlador extends VistasModelo{
         /*---Controlador para obtener plantilla---*/ 
-        
+        public function obtener_platilla_modelo(){
+            return require_once "./vistas/plantilla.php";
+        }
+        /*---Controlador para obtener vistas---*/ 
+        public function obtener_platilla_controlador(){
+            if(isset($_GET['views'])){
+                $ruta=explode("/",$_GET['views']);
+                $respuesta=VistasModelo::obtener_vistas_modelo($ruta[0]);
+            }else{
+                return $respuesta="login";
+            }
+            return $respuesta;
+        }
+
     }
+    ?>
