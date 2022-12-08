16e7a09f73ee6c1811a4d9830eb87f7a810c9a8f
Kurumi-007 <fly07sal@gmail.com>
2022-12-08 15:43:02 -0600

    creacion de vistas y eliminacion de archivos

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -2,7 +2,7 @@
     class VistasModelo{
         /*--- Modelo para obtener vistas ---*/
         protected static function obtener_vistas_modelo($vistas){
-            $listaBlanca=[];
+            $listaBlanca=["home"];
             if(in_array($vistas, $listaBlanca)){
                 if(is_file("./vistas/contenido/".$vistas."-view.php")){
                     $contenido="./vistas/contenido/".$vistas."-view.php";
